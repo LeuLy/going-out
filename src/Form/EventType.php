6de4cd9df509504a095f3c50f8c093d74e2b9eb8
LydiaC <cornu_lydia@hotmail.fr>
2020-02-11 11:22:17 +0100

    Modal Bootstrap New Place

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 //use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -84,6 +85,13 @@ class EventType extends AbstractType
             ->add (
                 'place',
                 PlaceType::class
+            )
+            ->add(
+                    'save',
+                    SubmitType::class,
+                    [
+                            'label' => 'Envoyer',
+                    ]
             )
         ;
     }
