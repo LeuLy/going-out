96bf230a6b3e565d9f892695d42674902e9cfe23
baptiste <baptiste.laz@gmail.com>
2020-02-12 17:22:13 +0100

    creation checkbox v1

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -82,60 +82,60 @@ DQL;
     }
 
 
-    public function findEventByDate($endDate, $beginDate, $var, $site, $page = 0, $limit = 100)
-    {
-
-
-        $entityManager = $this->getEntityManager();
-        $dql           = <<<DQL
-SELECT e
-FROM APP\ENTITY\Event e
-WHERE e.site          = :site
-AND e.dateStart BETWEEN :beginDate AND :endDate
-DQL;
-
-        $query = $entityManager
-            ->createQuery($dql)
-            ->setParameter(':endDate', $endDate)
-            ->setParameter(':beginDate', $beginDate)
-            ->setParameter(':site', $site)
-            ->setParameter(':var', "%".$var."%")
-            ->setFirstResult($page * $limit)
-            ->setMaxResults($limit);
-
-        $paginator = new Paginator($query, true);
-
-        return $paginator;
-
-
-    }
-
-
-    public function findEventByDescription($var, $site, $page = 0, $limit = 100)
-    {
-
-
-        $entityManager = $this->getEntityManager();
-        $dql           = <<<DQL
-SELECT e
-FROM APP\ENTITY\Event e
-WHERE e.site            = :site
-AND (e.label            LIKE :var
-OR e.description        LIKE :var)
-DQL;
-
-        $query = $entityManager
-            ->createQuery($dql)
-            ->setParameter(':site', $site)
-            ->setParameter(':var', "%".$var."%")
-            ->setFirstResult($page * $limit)
-            ->setMaxResults($limit);
-
-        $paginator = new Paginator($query, true);
-
-        return $paginator;
-
-    }
+//    public function findEventByDate($endDate, $beginDate, $var, $site, $page = 0, $limit = 100)
+//    {
+//
+//
+//        $entityManager = $this->getEntityManager();
+//        $dql           = <<<DQL
+//SELECT e
+//FROM APP\ENTITY\Event e
+//WHERE e.site          = :site
+//AND e.dateStart BETWEEN :beginDate AND :endDate
+//DQL;
+//
+//        $query = $entityManager
+//            ->createQuery($dql)
+//            ->setParameter(':endDate', $endDate)
+//            ->setParameter(':beginDate', $beginDate)
+//            ->setParameter(':site', $site)
+//            ->setParameter(':var', "%".$var."%")
+//            ->setFirstResult($page * $limit)
+//            ->setMaxResults($limit);
+//
+//        $paginator = new Paginator($query, true);
+//
+//        return $paginator;
+//
+//
+//    }
+//
+//
+//    public function findEventByDescription($var, $site, $page = 0, $limit = 100)
+//    {
+//
+//
+//        $entityManager = $this->getEntityManager();
+//        $dql           = <<<DQL
+//SELECT e
+//FROM APP\ENTITY\Event e
+//WHERE e.site            = :site
+//AND (e.label            LIKE :var
+//OR e.description        LIKE :var)
+//DQL;
+//
+//        $query = $entityManager
+//            ->createQuery($dql)
+//            ->setParameter(':site', $site)
+//            ->setParameter(':var', "%".$var."%")
+//            ->setFirstResult($page * $limit)
+//            ->setMaxResults($limit);
+//
+//        $paginator = new Paginator($query, true);
+//
+//        return $paginator;
+//
+//    }
 
 
 
